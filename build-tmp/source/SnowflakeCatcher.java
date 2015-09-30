1613097626cbc38b9d7f256b198632384e4f07e8
unknown <tholsen@697-334-Boromir.sf.k12.internal>
2015-09-30 08:33:34 -0700

    progress

--- build-tmp/source/SnowflakeCatcher.java
+++ build-tmp/source/SnowflakeCatcher.java
@@ -1,16 +1,31 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
 
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SnowflakeCatcher extends PApplet {
+
+Snowflake a = new Snowflake();
 catcher [] line = new catcher[1000];
 Snowflake [] storm;
 int counter=0;
-void setup()
+public void setup()
 {
   //your code here
-  background(0);
   size(300, 300);
   stormMaker();
   noStroke();
 }
-void draw()
+public void draw()
 {
   background(0);
   for(int i = 0;i<line.length;i++){
@@ -19,21 +34,23 @@ void draw()
     }
   }
   for(int i = 0;i<storm.length;i++){
-    storm[i].erase();
-    storm[i].lookDown();
+    
+   storm[i].show(); 
     storm[i].move();
     storm[i].wrap();
-    storm[i].show();
-    
+    storm[i].lookDown();
   }
   
-  
+  a.show();
+  a.move();
+  a.wrap();
+  a.lookDown();
   if(counter>997){
     text("you ran out of ink!",200,200);
   }
   //your code here
 }
-void mouseDragged()
+public void mouseDragged()
 {
   //your code here
   fill(40,40,0);
@@ -42,7 +59,7 @@ void mouseDragged()
   };
   counter++;
 }
-void stormMaker()
+public void stormMaker()
 {
   storm=new Snowflake[500];
   for(int i=0;i<storm.length;i++){
@@ -58,7 +75,7 @@ class catcher
    this.y=y;
 
   }
-  void show(){
+  public void show(){
     fill(157,157,162);
     ellipse(x,y,4,4);
 
@@ -75,43 +92,40 @@ class Snowflake
     this.x=(int)(Math.random()*297+2);
     //this.y=0;
     this.y=(int)(Math.random()*297+2);
-    //this.y=0;
     isMoving=true;
     //class member variable initializations
   }
-  void show()
+  public void show()
   {
     //your code here
-    noStroke();
-    fill(220,220,220);
+    fill(255,255,255);
     ellipse(x, y, 5, 5);
   }
-  void lookDown()
+  public void lookDown()
   {
     
     //your code here
     if(y>0&&y<290){
-      if(get(x,y+4) != color(0,0,0) && get(x,y+4) != color(220,220,220))
+      if(get(x,y+2) != color(0,0,0) && get(x,y+2) != color(255,255,255))
         isMoving=false;
     }
   }
-  void erase()
+  public void erase()
   {
     //your code here
-    fill(0);
-    ellipse(x,y,8,8);
+    fill(255);
   }
-  void move()
+  public void move()
   {
     //your code here
     if(isMoving==true){
       this.y++;
     }
   }
-  void wrap()
+  public void wrap()
   {
-    if(this.y>300){
-      this.y=1;
+    if(this.y>310){
+      this.y=0;
       this.x=(int)(Math.random()*297+2);
     }
     //your code here
@@ -119,3 +133,12 @@ class Snowflake
 }
 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SnowflakeCatcher" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
